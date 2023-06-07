fd0d2b2a63cd3b488cc0a9437c0b09e14944b3db
Angela Lima <angelamcostalima@icloud.com>
2023-06-07 23:48:22 +0100

    :recycle: change the order of $(SRC) and $(FLAGS)

--- MakefileGenerator.hpp
+++ MakefileGenerator.hpp
@@ -4,7 +4,7 @@
 # define CC "CC\t\t=\tcc\nFLAGS\t=\t-Wall -Werror -Wextra\n\n"
 # define CPP "CC\t\t=\tg++\nFLAGS\t=\t-Wall -Werror -Wextra -std=c++98\n\n"
 # define ALL "all:\t\t$(NAME)\n\n"
-# define MAKE "$(NAME):\n\t\t\t$(CC) $(FLAGS) $(SRC) -o $(NAME)\n\n"
+# define MAKE "$(NAME):\n\t\t\t$(CC) $(SRC) $(FLAGS) -o $(NAME)\n\n"
 # define CLEAN "clean:\n\t\t\t$(RM) $(NAME)\n\n"
 # define FCLEAN "fclean:\t\tclean\n\n"
 # define RE "re:\n\t\t\t@$(MAKE) fclean\n\t\t\t@$(MAKE)"
